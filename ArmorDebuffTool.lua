c454e84975ed5b427ef0ac88a9bff547d1276615
knasen88 <ronnan@live.se>
2018-01-23 22:13:46 +0100

    armor calc fix

--- ArmorDebuffTool.lua
+++ ArmorDebuffTool.lua
@@ -13,58 +13,42 @@ function checkDebuffs()
   ArmorDebuffTool:SetBackdropColor(0, 0, 0, 0.5)
   SunderStacks = 0
   ShatterStacks = 0
-  --multiplier = 1
   MinusArmor = 1
-  --MinusArmor = MinusArmor - 1
+
 
   for i=1,16 do local name, stack = UnitDebuff("target", i)
     if name == "Interface\\Icons\\Spell_Nature_FaerieFire" then -- Check for faeriefire
       ArmorDebuffTool.ff:SetTextColor(0, 1, 0, 1)
-      --multiplier = multiplier * 1.2
 	  MinusArmor = MinusArmor + 505
-	  MinusArmor = MinusArmor - 1
     end
     if name == "Interface\\Icons\\inv_axe_12" then -- Check Annajalaijtor stacks
 		ShatterStacks = SAstack
 		MinusArmor = MinusArmor * (1+200*ShatterStacks)
-		MinusArmor = MinusArmor - 1
+		--MinusArmor = MinusArmor - 1
 		if ShatterStacks < 3 then
 			ArmorDebuffTool.ann:SetTextColor(1, 1, 0, 1)
 		else
 			ArmorDebuffTool.ann:SetTextColor(0, 1, 0, 1)
-      --ArmorDebuffTool.nf:SetTextColor(0, 1, 0, 1)
-      --multiplier = multiplier * 1.15
-	  
 	  end
     end
     if name == "Interface\\Icons\\Ability_Warrior_Sunder" then -- Check sunders
       SunderStacks = stack
-	  MinusArmor = MinusArmor * (1+450*SunderStacks)
-	  MinusArmor = MinusArmor - 1
+	  MinusArmor = MinusArmor + (450*SunderStacks)
+	  MinusArmor = MinusArmor - 1 --most likely to be up so it fixes the wrong value from the start
       if SunderStacks < 5 then
         ArmorDebuffTool.sa:SetTextColor(1, 1, 0, 1)
       else
         ArmorDebuffTool.sa:SetTextColor(0, 1, 0, 1)
-      
-      --multiplier = multiplier * (1+0.03*SunderStacks)
-	  
 	  end
     end
 	if name == "Interface\\Icons\\Spell_Shadow_UnholyStrength" then -- Check curse of reck
       ArmorDebuffTool.cor:SetTextColor(0, 1, 0, 1)
-      --multiplier = multiplier * 1.1
 	  MinusArmor = MinusArmor + 640
-	  MinusArmor = MinusArmor - 1
+	  --MinusArmor = MinusArmor - 1
 	end
---MinusArmor = MinusArmor - 1
-  --multiplier = (multiplier * 100) - 100
-  --multiplier = math.floor(multiplier + 0.5)
   ArmorDebuffTool.baseText:SetText(base .. MinusArmor .."")
-  --if multiplier > 100 then
-    --ArmorDebuffTool.SetBackdropColor(0, 1, 0, 1)
-  --end
   end
-  --MinusArmor = MinusArmor - 1
+ 
 end
 
 local backdrop = {
@@ -72,10 +56,10 @@ local backdrop = {
 }
 
 base = "Armor reduced:    "
-SunderStackstacks = 0
+SunderStacks = 0
 ShatterStacks = 0
 MinusArmor = 0
---multiplier = 0
+
 
 -- Sets up basic frame stuff
 ArmorDebuffTool = CreateFrame('Frame')
@@ -135,20 +119,4 @@ ArmorDebuffTool:RegisterEvent('PLAYER_TARGET_CHANGED')
 
 --  TODO
 -- Clean up and fix everything
--- Add flashing Icons?
-
--- List of Things to check for
-
--- Warlocks: 1*1.2*1.1*1.15*1.15
--- Shadow Vuln: Spell_Shadow_ShadowBolt
--- CoS: Spell_Shadow_CurseOfAchimonde
--- Nightfall: Spell_Holy_ElunesGrace
--- Shadow Weaving (5 stacks):
---
-
--- Mages:
--- CoE:
--- Nightfall:
--- Winter's Chill (Frost) (5 stacks):
--- Improved Scorch (Fire) (5 stacks):
--- Ignite:
+--fix movable frame from ingame
